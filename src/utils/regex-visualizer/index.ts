1980b5a5498607ad5518f835b96d55ee85d24e8e
fumtas1k <fumta01a7@gmail.com>
2026-05-27 10:18:25 +0900

    feat: regexp-tree AST から鉄道図レイアウトを組む buildRailroad を追加

--- src/utils/regex-visualizer/index.ts
+++ src/utils/regex-visualizer/index.ts
@@ -1,2 +1,4 @@
 export { parseRegex, type RegexAstNode } from './parse';
 export { analyzeRedos, type RedosResult, type RedosStatus } from './redos';
+export { buildRailroad } from './railroad';
+export type { RailNode } from './railroad-layout';
